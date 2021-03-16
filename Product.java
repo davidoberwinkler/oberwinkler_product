a50370a3471ec2ad9c2f43939f4d099d37fe9e0e
David <d.oberwinkler@gmx.at>
2021-03-16 10:17:20 +0100

    Product

---
+++ Product.java
@@ -0,0 +1,14 @@
+public class Product{
+
+ private String name = "Coca Cola";
+ public Product(String name) {
+  	this.name = name;
+ }
+ public void setname(String name){ 
+	this.name = name; 
+ }
+ public String getname(){ 
+	return name;
+ }
+
+}
