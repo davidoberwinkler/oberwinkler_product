2d1a6ca2a8f88b4700974bdf2bedbe1f58ff09f8
Fritz <fritz@spengergasse.at>
2021-03-21 11:29:11 +0100

    change

--- Product.java
+++ Product.java
@@ -2,9 +2,11 @@ public class Product{
 
  private String name = "Coca Cola";
  private float price = 10
- public Product(String name, float price) {
+ private int id;
+ public Product(String name, float price, int id) {
   	this.name = name;
 	this.price = price;
+	this.id = id;
  }
  public void setname(String name){ 
 	this.name = name; 
@@ -17,5 +19,11 @@ public class Product{
  }
  public float getprice(){ 
 	return price;
+ }
+ public void setid(int id){ 
+	this.id = id; 
+ }
+ public int getid(){ 
+	return id;
  }
 }
